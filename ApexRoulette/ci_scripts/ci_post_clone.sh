645d741cca566f156365e8a8203ec0e79f6ff2e1
daikiumehara <daikiumehara3@gmail.com>
2024-02-21 16:48:42 +0900

    add: ci script

---
+++ ApexRoulette/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  ApexRoulette
+#
+#  Created by daiki umehara on 2024/02/21.
+#  Copyright © 2024 Daiki Umehara. All rights reserved.
+
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
